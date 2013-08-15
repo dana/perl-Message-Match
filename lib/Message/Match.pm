d1b5fadc2d3758cff8deac2c4e2b88f017fe0540
Dana M. Diederich <diederich@gmail.com>
2013-08-14 17:46:39 -0700

    moved to Dist::Zilla

--- lib/Message/Match.pm
+++ lib/Message/Match.pm
@@ -2,11 +2,9 @@ package Message::Match;
 
 use strict;use warnings;
 require Exporter;
-use vars qw($VERSION @ISA @EXPORT_OK);
+use vars qw(@ISA @EXPORT_OK);
 @ISA = qw(Exporter);
 @EXPORT_OK = qw(mmatch);
-
-$VERSION = '0.3';
 
 sub mmatch {
     my ($message, $match) = @_;
